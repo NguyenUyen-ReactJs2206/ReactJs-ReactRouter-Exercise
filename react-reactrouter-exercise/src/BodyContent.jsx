122165a88e320ad35ad93165e19ed09470911d90
Uyen <uyenntt17407@st.uel.edu.vn>
2022-10-24 00:11:43 +0700

    Full ReadMore

--- react-reactrouter-exercise/src/BodyContent.jsx
+++ react-reactrouter-exercise/src/BodyContent.jsx
@@ -4,29 +4,25 @@ import "./Header.css";
 
 export default function BodyContent() {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-//   const [readMore,setIsReadMore] = useState(false);
-
+  const listData = data.articles;
+  const [readMore, setIsReadMore] = useState(false);
+  const handleLess = () => {
+    setIsReadMore(false);
+  };
+  const handleMore = () => {
+    setIsReadMore(true);
+  };
 
   useEffect(() => {
-    setIsLoading(true);
-    getListUserAxios()
-      .then((result) => {
-        setData((pre) => (pre = result));
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setData([]);
-        setIsLoading(false);
-      });
+    getListUserAxios().then((result) => {
+      setData((pre) => (pre = result));
+    });
   }, []);
-const listData = data.articles;
+
   // class component shouldComponentUpdate =>
 
   return (
     <div>
-       
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
@@ -38,30 +34,56 @@ const listData = data.articles;
               </div>
             </div>
             <hr />
-            {
-           listData.map((el,index)=>(
-                <div className="article-preview" key={index}>
+            {listData.map((el, index) => (
+              <div className="article-preview" key={index}>
                 <div className=" container-fluid infor-article">
                   <div className="row">
                     <div className="col-sm-1">
-                      <img src={el.author.image} alt="#" className="image-infor" />
+                      <img
+                        src={el?.author?.image}
+                        alt="#"
+                        className="image-infor"
+                      />
                     </div>
                     <div className="col-sm-8 infor-sender">
-                      <h5 >{el.author.username}</h5>
-                      <h6>{el.createdAt}</h6>
+                      <h5>{el?.author?.username}</h5>
+                      <h6>{el?.createdAt}</h6>
                     </div>
-                    <div className="col-sm-3 favoritescount">{el.favoritesCount}</div>
+                    <div className="col-sm-3 favoritescount">
+                      <button type="button" className="btn btn-secondary">
+                        {el?.favoritesCount}
+                      </button>
+                    </div>
                   </div>
                 </div>
                 <div className="container-fluid preview">
-                  <h2> {el.title}</h2>
-                  <p>{el.body}</p>
-                  {/* <span ? setIsReadMore({el.title}) : setIsReadMore("...") >ReadMore</span> */}
+                  <p className="content redless" style={{display : readMore ? "block" : "none"}}>
+                    <h2> {el?.title}</h2>
+                    <p>{el?.body}</p>
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={() => handleLess()}
+                    >
+                      ReadLess
+                    </button>
+                  </p>
+                  <p className="readmore" style={{display : readMore ? "none" : "block"}}>
+                    <h2>
+                      {el?.title.slice(0, 100)}... 
+                   
+                    <button
+                        type="button"
+                        onClick={() => handleMore()}
+                        className="btn btn-primary"
+                      >
+                        ReadMore 
+                      </button>
+                    </h2>
+                  </p>
                 </div>
-                <button type="button" class="btn btn-secondary">Load more</button>
               </div>
-            ))
-        } 
+            ))}
           </div>
           <div className="col-md-3">
             <div className="slide-bar">
@@ -69,7 +91,7 @@ const listData = data.articles;
               <div className="tag-list">
                 <a href="#"> implemntations</a>
                 <a href="#"> welcome</a>
-                <a href="#"> introduction</a> 
+                <a href="#"> introduction</a>
                 <a href="#"> ipsum</a>
               </div>
             </div>
